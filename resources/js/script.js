2af6209e6822a3d785337cf78e57b9f412c206cb
Neamul-karim <neamulkarim1994@gmail.com>
2024-08-26 17:43:18 +0600

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,10 @@
+// HAMBURG MENU
+function openMenu(){
+    document.getElementById("navber").style.height = "100%";
+}
+function closeMenu(){
+    document.getElementById("navber").style.height = "0%";
+}
+
+// MIXITUP
+var mixer = mixitup('.work-grid');
